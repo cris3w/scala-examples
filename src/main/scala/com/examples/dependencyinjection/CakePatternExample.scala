465a1054ff28bb8cda225f8d144cf122cdef91f7
Cristina Delgado <cris3wdev@gmail.com>
2017-06-07 20:41:04 +0200

    Add example of Cake Pattern

---
+++ src/main/scala/com/examples/dependencyinjection/CakePatternExample.scala
@@ -0,0 +1,52 @@
+package com.examples.dependencyinjection
+
+
+// example based on a Jonas Boner's post:
+// http://jonasboner.com/real-world-scala-dependency-injection-di/
+
+
+case class User(username: String, password: String)
+
+
+trait UserRepositoryComponent {
+
+  val userRepository: UserRepository
+
+  class UserRepository {
+
+    def authenticate(user: User): User = {
+      println(s"Authenticating user: ${user.username}")
+      user
+    }
+  }
+}
+
+
+trait UserServiceComponent {
+
+  // using self-type annotation to declare the dependencies this component requires
+  this: UserRepositoryComponent =>
+
+  class UserService {
+
+    def authenticate(username: String, password: String): User =
+      userRepository.authenticate(User(username, password))
+  }
+}
+
+
+object ComponentRegistry
+  extends UserServiceComponent with UserRepositoryComponent {
+
+  val userRepository = new UserRepository
+  val userService = new UserService
+}
+
+
+object CakePatternExample extends App {
+
+  import ComponentRegistry._
+
+  userService.authenticate("admin", "user")
+
+}
