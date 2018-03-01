260462963c3e8a9efbc3973c5965c66593134f70
Cristina Delgado <cris3wdev@gmail.com>
2018-03-01 20:13:35 +0100

    Add example TrafficLight using MonadState

---
+++ src/main/scala/com/examples/ScalaZ/Monads/MonadState/TrafficLightExample.scala
@@ -0,0 +1,89 @@
+package com.examples.ScalaZ.Monads.MonadState
+
+
+// example of Timothy Perrett's blog:
+// http://timperrett.com/2013/11/25/understanding-state-monad/
+
+
+sealed trait Aspect
+case object Green extends Aspect
+case object Amber extends Aspect
+case object Red   extends Aspect
+
+
+sealed trait Mode
+case object Off      extends Mode
+case object Flashing extends Mode
+case object Solid    extends Mode
+
+
+case class Signal(
+  isOperational: Boolean,
+  display: Map[Aspect, Mode])
+
+
+object Signal {
+
+  import scalaz.State, State._
+
+
+  type ->[A, B] = (A, B)
+  type SignalState[A] = State[Signal, A]
+
+
+  val default = Signal(
+    isOperational = false,
+    display = Map(Red -> Flashing, Amber -> Off, Green -> Off))
+
+
+  def enable: State[Signal, Boolean] =
+    for {
+      a <- init
+      _ <- modify((s: Signal) => s.copy(isOperational = true))
+      r <- get
+    } yield r.isOperational
+
+
+  def halt  = change(Red -> Solid, Amber -> Off,   Green -> Off)
+  def ready = change(Red -> Solid, Amber -> Solid, Green -> Off)
+  def go    = change(Red -> Off,   Amber -> Off,   Green -> Solid)
+  def slow  = change(Red -> Off,   Amber -> Solid, Green -> Off)
+
+
+  private def change(seq: Aspect -> Mode*): State[Signal, Map[Aspect, Mode]] =
+    for {
+      m <- init
+      _ <- modify(display(seq))
+      signal <- get
+    } yield signal.display
+
+
+  private def display(seq: Seq[Aspect -> Mode]): Signal => Signal = signal =>
+    if (signal.isOperational)
+      signal.copy(display = signal.display ++ seq.toMap)
+    else default
+
+}
+
+
+object TrafficLightExample extends App {
+
+  import Signal._
+  import scalaz.State.{get => current}
+
+  val program = for {
+    _  <- enable
+    r0 <- current
+    _  <- halt
+    r1 <- current
+    _  <- ready
+    r2 <- current
+    _  <- go
+    r3 <- current
+    _  <- slow
+    r4 <- current
+  } yield r0 :: r1 :: r2 :: r3 :: r4 :: Nil
+
+  program.eval(default).zipWithIndex.foreach { case (v, i) => println(s"r$i - $v") }
+
+}
